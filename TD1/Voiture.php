64f75cd7e10f8b2244cfc877802e288bcb442d78
Numa MAURIN <numa.maurin@etu.umontpellier.fr>
2019-09-05 17:28:53 +0200

    debut get et set de couleur

---
+++ TD1/Voiture.php
@@ -0,0 +1,38 @@
+<?php
+class Voiture {
+   
+  private $marque;
+  private $couleur;
+  private $immatriculation;
+      
+  // un getter      
+  public function getMarque() {
+       return $this->marque;  
+  }
+     
+  // un setter 
+  public function setMarque($marque2) {
+       $this->marque = $marque2;
+  }
+      
+  // un constructeur
+  public function __construct($m, $c, $i)  {
+   $this->marque = $m;
+   $this->couleur = $c;
+   $this->immatriculation = $i;
+  } 
+           
+  // une methode d'affichage.
+  public function afficher() {
+    // À compléter dans le prochain exercice
+  }
+
+  public function getcouleur(){
+    return $this->couleur;
+  }
+
+  public function setcouleur( $newcolor){
+    $this->couleur=$newcolor
+  }
+}
+?>
