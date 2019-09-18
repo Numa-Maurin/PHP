98f65c34e13ef3c9e39bbb7f71871028307cb159
Numa MAURIN <numa.maurin@etu.umontpellier.fr>
2019-09-18 10:40:50 +0200

    modif exo 7

--- TD1/Voiture.php
+++ TD1/Voiture.php
@@ -16,11 +16,16 @@ class Voiture {
   }
       
   // un constructeur
-  public function __construct($m, $c, $i)  {
-   $this->marque = $m;
-   $this->couleur = $c;
-   $this->immatriculation = $i;
-  } 
+  public function __construct($m = NULL, $c = NULL, $i = NULL) {
+    if (!is_null($m) && !is_null($c) && !is_null($i)) {
+      // Si aucun de $m, $c et $i sont nuls,
+      // c'est forcement qu'on les a fournis
+      // donc on retombe sur le constructeur à 3 arguments
+      $this->marque = $m;
+      $this->couleur = $c;
+      $this->immatriculation = $i;
+    }
+  }
            
   // une methode d'affichage.
   public function afficher() {
