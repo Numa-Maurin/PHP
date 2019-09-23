ee6a29a3c66606549291cd8b72e94544e9ae2fc9
Numa MAURIN <numa.maurin@etu.umontpellier.fr>
2019-09-23 13:35:52 +0200

    fin exo 2

--- TD1/Trajet.php
+++ TD1/Trajet.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'Model.php';
 class Trajet {
    private $id;
    private $depart;
@@ -7,7 +8,6 @@ class Trajet {
    private $nbplaces;
    private $prix;
    private $conducteur_login;
-   }
 
 public function get($nom_attribut){
 	return $this->$nom_attribut;
@@ -19,13 +19,32 @@ public function set($nom_attribut,$value){
 
 public function __construct($data)
 {
-	$this->$id=$data["id"];
-	$this->$=$data["depart"];
-	$this->$=$data["arrivee"];
-	$this->$=$data["date"];
-	$this->$=$data["nbplaces"];
-	$this->$=$data["prix"];
-	$this->$=$data["conducteur_login"];
+	if (!is_null($data["id"]) && (!is_null($data["depart"]) && (!is_null($data["arrivee"]) && (!is_null($data["date"]) && (!is_null($data["nbplaces"]) && (!is_null($data["prix"]) && (!is_null($data["conducteur_login"])) {
+		$this->$id=$data["id"];
+		$this->$=$data["depart"];
+		$this->$=$data["arrivee"];
+		$this->$=$data["date"];
+		$this->$=$data["nbplaces"];
+		$this->$=$data["prix"];
+		$this->$=$data["conducteur_login"];
+	}
+}
+
+    public static function getAllUtilisateurs()
+  {
+      $sql='SELECT * FROM trajet';
+      $rep=(Model::$pdo)->query($sql);
+      $rep->setFetchMode(PDO::FETCH_CLASS, 'Trajet');
+      $tab_trajet = $rep->fetchAll();
+      foreach ($tab_trajet as $key1 => $value1) {
+        $value1->afficherTrajet();
+    }
+  }
+
+    public function afficherTrajet() {
+    echo "id trajet : $this->id, depart : $this->depart, arrivee : $this->arrivee, date : $this->date, nbplaces : $this->nbplaces, prix : $this->prix, conducteur_login : $this->conducteur_login" ;
+  }
+
 }
 
 ?>
--- TD1/Utilisateur.php
+++ TD1/Utilisateur.php
@@ -1,9 +1,32 @@
 <?php
+require_once 'Model.php';
 class Utilisateur {
    private $login;
    private $nom;
    private $prenom;
 
-   }
+  public function __construct($l = NULL, $n = NULL, $p = NULL) {
+    if (!is_null($l) && !is_null($n) && !is_null($p)) {
+      $this->login = $l;
+      $this->nom = $n;
+      $this->penom = $p;
+    }
+  }
 
+    public static function getAllVoitures()
+  {
+      $sql='SELECT * FROM utilisateur';
+      $rep=(Model::$pdo)->query($sql);
+      $rep->setFetchMode(PDO::FETCH_CLASS, 'Utilisateur');
+      $tab_utilisateur = $rep->fetchAll();
+      foreach ($tab_utilisateur as $key1 => $value1) {
+        $value1->afficher();
+    }
+  }
+
+    public function afficher() {
+    echo "id utilisateur : $this->id, nom : $this->nom, prenom : $this->prenom" ;
+  }
+ 
+}
 ?>
