77cd509b87c57a97e8994ab2506410eee1a681ad
Numa MAURIN <numa.maurin@etu.umontpellier.fr>
2019-09-25 12:15:18 +0200

    TD4 exercice 6

---
+++ TD4/controller/controllerVoiture.php
@@ -0,0 +1,9 @@
+<?php
+require_once ('../model/ModelVoiture.php'); // chargement du modèle
+class controllerVoiture {
+    public static function readAll() {
+        $tab_v = ModelVoiture::getAllVoitures();     //appel au modèle pour gerer la BD
+        require ('../view/voiture/list.php');  //"redirige" vers la vue
+    }
+}
+?>
