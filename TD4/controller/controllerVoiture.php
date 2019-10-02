3f0836caec9b0c1e9d3ffc3961ae13ab50142b53
Numa MAURIN <numa.maurin@etu.umontpellier.fr>
2019-10-02 11:23:34 +0200

    fin TD4 (partie bonus a faire)

--- TD4/controller/controllerVoiture.php
+++ TD4/controller/controllerVoiture.php
@@ -5,5 +5,27 @@ class controllerVoiture {
         $tab_v = ModelVoiture::getAllVoitures();     //appel au modèle pour gerer la BD
         require ('../view/voiture/list.php');  //"redirige" vers la vue
     }
+
+    public static function read() {
+        $v = ModelVoiture::getVoitureByImmat($_GET["immat"]);     //appel au modèle pour gerer la BD
+        if ($v==false) 
+        	require ('../view/voiture/error.php');  //"redirige" vers la vue d'erreur
+        else
+        	require ('../view/voiture/detail.php');  //"redirige" vers la vue
+    }
+
+    public static function create(){
+    	require ('../view/voiture/create.php');
+    }
+
+    public static function created(){
+    	$immat =$_GET["immatriculation"];
+    	$couleur =$_GET["couleur"];
+    	$marque =$_GET["marque"];
+    	$v1=new ModelVoiture($marque,$couleur,$immat);
+    	$v1->save();
+		ControllerVoiture::readAll();
+    }
+
 }
 ?>
---
+++ TD4/view/voiture/create.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Formulaire</title>
+    </head>
+   
+    <body>
+
+         <form method="GET">
+          <fieldset>
+            <legend>Mon formulaire :</legend>
+            <p>
+              <input type='hidden' name='action' value='created'>
+
+              <label for="immat_id">Immatriculation</label>
+              <input type="text" placeholder="Ex : 256AB34" name="immatriculation" id="immat_id" required/>
+              <label for="couleur_id">Couleur</label> :
+              <input type="text" placeholder="Ex : GRISE" name="couleur" id="couleur_id" required/>
+              <label for="marque_id">Marque</label> :
+              <input type="text" placeholder="Ex : renault" name="marque" id="marque_id" required/>
+              
+            </p>
+            <p>
+              <input type="submit" value="Envoyer" />
+            </p>
+          </fieldset> 
+        </form>
+
+        <?php
+
+          ?>
+    </body>
+</html> 
