d6a0d296f6abcf999dd94be8e72498d338522dbb
Numa MAURIN <numa.maurin@etu.umontpellier.fr>
2019-11-12 15:14:14 +0100

    mise en ligne bien

--- TD7/view/voiture/detail.php
+++ TD7/view/voiture/detail.php
@@ -10,9 +10,11 @@
             echo '<p> Voiture de couleur ' . $vCouleur. '.</p>';
             echo '<p> Voiture de marque ' . $vMarq. '.</p>';
 
-            echo '<p><a href="http://webinfo/~maurinn/PHP/TD7/index.php?action=delete&immat='.$vImmat.'">SUPPRIMER CETTE VOITURE</a>.</p>';
+            echo '<p><a href="
+http://webinfo.iutmontp.univ-montp2.fr/~maurinn/PHP/TD7/index.php?action=delete&immat='.$vImmat.'">SUPPRIMER CETTE VOITURE</a>.</p>';
 
-            echo '<p><a href="http://webinfo/~maurinn/PHP/TD7/index.php?action=update&immat='.$vImmat.'">MODIFIER CETTE VOITURE</a>.</p>';
+            echo '<p><a href="
+http://webinfo.iutmontp.univ-montp2.fr/~maurinn/PHP/TD7/index.php?action=update&immat='.$vImmat.'">MODIFIER CETTE VOITURE</a>.</p>';
         ?>
     </body>
 </html>
--- TD7/view/voiture/list.php
+++ TD7/view/voiture/list.php
@@ -5,7 +5,7 @@
         foreach ($tab_v as $v){
             $vImmatHTML= htmlspecialchars($v->getImmatriculation());
             $vImmatURL=rawurlencode($v->getImmatriculation());
-            echo '<p> Voiture d\'immatriculation <a href="http://webinfo/~maurinn/PHP/TD7/index.php?action=read&immat='.$vImmatURL.'">'.$vImmatHTML.'</a>.</p>';
+            echo '<p> Voiture d\'immatriculation <a href="http://webinfo.iutmontp.univ-montp2.fr/~maurinn/PHP/TD7/index.php?action=read&immat='.$vImmatURL.'">'.$vImmatHTML.'</a>.</p>';
         }
         ?>
     </body>
